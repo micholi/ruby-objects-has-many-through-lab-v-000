283675a576ee6245e5a5549abf2d798b518810f1
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 14:32:45 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,8 +8,6 @@ attr_accessor :name
   end
 
   def add_appointment(appointment)
-    # adds a new appointment to the doctor's @appointments array
-    # and tells that appointment that it belongs to the docts
     @appointments << appointment
     appointment.doctor = self
   end
@@ -19,7 +17,6 @@ attr_accessor :name
   end
 
   def patients
-    # has many patients, through appointments
     self.appointments.collect do |appointment|
       appointment.patient
     end
