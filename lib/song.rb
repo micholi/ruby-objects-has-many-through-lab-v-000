bd4e1f14b79601309d1321b8b80b517b896ac463
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 13:23:41 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ attr_accessor :name, :genre, :artist
   def genre
     # belongs to a genre
     #genre.add_song(self)
-    genre.add_song(self)
-    self.genre = genre
+    #genre.add_song(self)
+    #self.genre = genre
   end
 
   def artist
