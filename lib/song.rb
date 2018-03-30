8c5aec60f119b90de01edc881d0c2cb0d06e11f4
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 13:30:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :genre, :artist
   def initialize(name, genre)
     @name = name
     self.genre = genre
-    #genre.add_song(self)
+    genre.add_song(self)
   end
 
   def genre
