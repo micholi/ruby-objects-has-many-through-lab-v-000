23fcf6e50446a861a3e086de7b47ac5e22c97a89
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 14:09:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,7 +10,7 @@ attr_accessor :name
   def add_appointment(appointment)
     # takes in an argument of an apppointment and adds that appointment to it's
     # list of appointments and tells that appointment it belongs to the patient
-
+    @appointments << appointment
   end
 
   def doctors
